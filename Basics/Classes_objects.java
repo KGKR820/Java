fd7f97eb9780a79f80395aad13fad969fece5155
KGKR <kgkr6018@gmail.com>
2025-07-30 22:05:24 +0530

    Added some Basics

---
+++ Basics/Classes_objects.java
@@ -0,0 +1,9 @@
+public class Classes_objects {
+    public class num{
+        int x = 5;
+    }
+    public static  void main(String args[]){
+         
+    }
+    
+}
---
+++ Basics/Math.java
@@ -0,0 +1,7 @@
+public class Math {
+    public static void main(String args[]){
+        System.out.println(java.lang.Math.max(4,7));
+        System.out.println(java.lang.Math.random());
+    }
+
+}
