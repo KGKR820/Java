6d64d6a0d87d0048ccda94c861095bb88ef55544
KGKR <kgkr6018@gmail.com>
2025-07-30 22:04:31 +0530

    First Commit

--- Basics/print.java
+++ Basics/print.java
@@ -1,7 +1,5 @@
-public class Hello_World {
-
+public class print {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hi Guys");
     }
 }
